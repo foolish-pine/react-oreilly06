490c6a867abd3b636fee445c30d086db9540562d
Matsuda Takuya <matsuda.takuya41@gmail.com>
2021-08-08 14:51:01 +0900

    commit

---
+++ src/components/StarRating.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { Star } from "./Star";
+
+export const StarRating = ({ totalStars = 5 }) => {
+  const [selectedStars, setSelectedStars] = useState(3);
+
+  return (
+    <>
+      {[...Array(totalStars)].map((n, i) => (
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+      <p>
+        {selectedStars} of {totalStars} stars
+      </p>
+    </>
+  );
+};
